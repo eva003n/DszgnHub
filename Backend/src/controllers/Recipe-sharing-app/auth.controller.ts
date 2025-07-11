afa00183e393e91da72b3d808fa9b2bbc73e6ce9
Evan ngugi wambui <evanngugi547@gmail.com>
2025-07-12 01:40:41 +0300

    Refactor, add email verification and MFA

--- Backend/src/controllers/Recipe-sharing-app/auth.controller.ts
+++ Backend/src/controllers/Recipe-sharing-app/auth.controller.ts
@@ -8,107 +8,281 @@ import type { NextFunction, Request, Response } from "express";
 import type {
   logInAuth,
   signUpAuth,
+  verificationCode,
 } from "../../middlewares/validators/Recipe-sharing-app/index.js";
 import type { ProtectedRequest } from "../../interfaces/Recipe-sharing-app/request.interface.js";
-import { ACCESS_TOKEN_EXPIRY, ACCESS_TOKEN_SECRET, EMAIL_ADDRESS, NODE_ENV, REFRESH_TOKEN_EXPIRY, REFRESH_TOKEN_SECRET } from "../../config/env.js"; 
+import {
+  ACCESS_TOKEN_EXPIRY,
+  ACCESS_TOKEN_SECRET,
+  API_DOC_URI,
+  EMAIL_ADDRESS,
+  FRONTEND_URL,
+  NODE_ENV,
+  REFRESH_TOKEN_EXPIRY,
+  REFRESH_TOKEN_SECRET,
+  VERIFICATIOB_URI,
+  VERIFICATION_TOKEN_EXPIRY,
+  VERIFICATION_TOKEN_SECRET,
+} from "../../config/env.js";
 import resend from "../../mail/index.js";
 import { APP_NAME } from "../../constants.js";
+import { randomInt } from "node:crypto";
+import * as speakeasy from "speakeasy";
+import { toDataURL } from "qrcode";
+import type { ObjectId } from "mongoose";
+import type { UserInterface } from "../../interfaces/Recipe-sharing-app/model.interface.js";
 
-const signUp = asyncHandler(async (req: ProtectedRequest, res: Response, next:NextFunction) => {
-  const { userName, email, password, confirmPassword } = req.body as signUpAuth;
-  // console.log(req.body)
+import { generateEmailTemplate } from "../../mail/template.js";
 
-  // check if user exist before creating new user
-  const isExistingUser = await User.findOne({
-    $or: [{ userName: userName }, { email: email }],
-  });
+const signUp = asyncHandler(
+  async (req: ProtectedRequest, res: Response, next: NextFunction) => {
+    const { userName, email, password } = req.body as signUpAuth;
+    // console.log(req.body)
 
-  //if user exist throw error
-  if (isExistingUser) {
-    return res
-      .status(409)
-      .json(ApiError.conflictRequest(409, "User already exist, please login"));
-  }
-  //if user does not exist
-  const newUser = await User.create({
-    userName,
-    email,
-    password,
-  });
+    const session = await User.startSession()
+    session.startTransaction()
 
-  //generate randon OTP
-  /*generateKey("hmac", { length: 30 }, async (err, key) => {
-        console.log(key.export().toString("hex"));
-        const code = key.export().toString("hex").toUpperCase();
-        sendMail(code, email);
-      });*/
-  //hide password and refresh token after making them visible in query so that it doesnt appear in response
-  newUser.password = undefined;
 
-  //aend email
-  // const { data, error } = await sendEmail(newUser.email, "Account creation", `Hello ${newUser.userName}, your account has been created successfully 🎉🎉`);
-  // if (error) 
-  //   return next(ApiError.internalServerError(500, error, error.message));
-  
+    // check if user exist before creating new user
+    const isExistingUser = await User.findOne({
+      $or: [{ userName: userName }, { email: email }],
+    }).session(session);
 
-  return res
-    .status(201)
-    .json(new ApiResponse(201, newUser, "Account created successfully"));
-});
-
-//Authentication
-const logIn = asyncHandler(async (req: Request, res: Response) => {
-  //user credentials, validate them before generating a token
-  const { email, password } = req.body as logInAuth;
-
-  //returns a doc that matches email and includes password field in query
-  const isExistingUser = await User.findOne({ email: email }).select(
-    "+refreshToken +email"
-  );
-  if (!isExistingUser) {
-    return res.status(404).json(ApiError.notFound(404, "User doesn't exist, create an account"));
-  }
-  //this wont work if passowrd is hidden
-  const isValidPassword = await isExistingUser.isMatchingPassword(password);
-
-  if (!isValidPassword) {
-    return res
-      .status(401)
-      .json(ApiError.unAuthorizedRequest(401, "Invalid username or password"));
-  }
-  //generate token for user session
-  const { accessToken, refreshToken } = generateToken(
-    isExistingUser._id,
-    isExistingUser.email
-  );
-
-  isExistingUser.refreshToken = refreshToken;
-  await isExistingUser.save();
-  //hide password and refresh token after making them visible in query so that it doesnt appear in response
-  isExistingUser.password = undefined;
-  isExistingUser.refreshToken = undefined;
-
-  //configure and send cookie
-  configureAndSendCookie(res, accessToken, refreshToken);
-
-  return res
-    .status(200)
-    .json(
-      new ApiResponse(
-        200,
-        { user: isExistingUser, accessToken },
-        "Logged in successfully"
-      )
+    //if user exist throw error
+    if (isExistingUser) {
+      return res
+        .status(400)
+        .json(
+          ApiError.badRequest(
+            400,
+            `${req.originalUrl}`,
+            "Account already exist, please login"
+          )
+        );
+    }
+    // const secret = randomInt(999999);
+    //generate a TOTP based 
+    const secret = speakeasy.generateSecret()
+    const code = secret.base32
+    //if user does not exist
+    const newUser = await User.create(
+      [
+        {
+          userName,
+          email,
+          password,
+          verificationSecret: code ,
+        },
+      ],
+      { session }
     );
-});
+if(newUser[0]){
+  newUser[0].password = undefined
+  newUser[0].verificationSecret = undefined;
+}
+  
+const token = speakeasy.totp({
+  secret: code,
+  encoding: "base32",
+  step: 300, //5 min
+})
+console.log(token)
+    const { data, error } = await sendEmail(
+      email,
+      "DezgnHubAPI",
+      "Verific]ation code",
+      token
+    );
+    if (error){
+      return next(
+        ApiError.internalServerError(500, `${req.originalUrl}`, error.message)
+      );
+    }
+      await session.commitTransaction()
+      await session.endSession()
+
+      //create user using clerk
+      // await clerkClient.users.createUser({username: userName, emailAddress: [email], password})
+    return res
+      .status(201)
+      .json(
+        new ApiResponse(
+          201,
+          newUser[0],
+          "Account created successfully, check verification code sent to email"
+        )
+      );
+  }
+);
+
+//verify account creation
+const verifyEmail = asyncHandler(
+  async (req: ProtectedRequest, res: Response, next: NextFunction) => {
+    const { verification_code, user_id } = req.body as verificationCode;
+    // const { VerificationToken } = req.cookies;
+
+    //verify verification code
+    const user = await User.findById(user_id).select("+verificationSecret")
+    if (!user)
+      return next(
+        ApiError.notFound(
+          404,
+          `${req.originalUrl}`,
+          "Account doesn't exist, please create an account"
+        )
+      );
+      if(!user.verificationSecret) throw new Error("Missing verification secret")
+        console.log(verification_code)
+    const verified = speakeasy.totp.verify({
+      secret: user.verificationSecret,
+      encoding: "base32",
+      token: verification_code,
+      step: 300 ,
+      window: 1 //allow slight time drift
+
+    })
+
+
+    if (!verified)
+      return next(
+        ApiError.badRequest(400, `${req.originalUrl}`, "Invalid verification code provided")
+      );
+
+//set email verification as completed
+    user.setIsVerified(true);
+    await user.save();
+    user.verificationSecret = undefined
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, user, "Email verified successfully"));
+  }
+);
+//Authentication
+const logIn = asyncHandler(
+  async (req: ProtectedRequest, res: Response, next: NextFunction) => {
+    //user credentials, validate them before generating a token
+    const { email, password } = req.body as logInAuth;
+
+    //returns a doc that matches email and includes password field in query
+    const isExistingUser = await User.findOne({ email: email }).select(
+      "+email +password + verificationSecret"
+    );
+
+    // isExistingUser && isExistingUser.maxRetry++;
+    // isExistingUser && await isExistingUser.save();
+
+    if (!isExistingUser) {
+      return next(
+        ApiError.notFound(
+          404,
+          `${req.originalUrl}`,
+          "Account doesn't exist, create an account"
+        )
+      );
+    }
+    //this wont work if passowrd is hidden
+    const isValidPassword = await isExistingUser.isMatchingPassword(password);
+
+    if (!isValidPassword) {
+      return next(
+        ApiError.unAuthorizedRequest(
+          401,
+          `${req.originalUrl}`,
+          "Invalid username or password"
+        )
+      );
+    }
+
+    //send twoFA qr code
+    const secret = speakeasy.generateSecret();
+    const code = secret.base32
+
+    isExistingUser.verificationSecret = code;
+    await isExistingUser.save();
+
+    isExistingUser.password = undefined;
+    isExistingUser.verificationSecret = undefined;
+
+     const dataUrl = await toDataURL(secret.otpauth_url as string);
+    return res
+      .status(200)
+      .setHeader("Content-Type", "text/html")
+      .send(
+        `<p style="text-align: center;">Scan the QR code for two-factor authentication</p>
+        <div style=" display: flex; justify-content: center;">
+        <img src="${dataUrl}" alt="qr-code for two-factor authentication" />
+        </div>
+        `
+      );
+  }
+);
+//Multifactor authentication using two-factor-auth
+const verifyMFA = asyncHandler(
+  async (req: ProtectedRequest, res: Response, next: NextFunction) => {
+    const { verification_code: token, user_id } = req.body as verificationCode;
+    const user = await User.findById(user_id).select(
+      "+verificationSecret +refreshToken +email"
+    );
+    if (!user)
+      return next(
+        ApiError.notFound(400, req.originalUrl, "User does not exist")
+      );
+if(!user.verificationSecret) throw new Error("Missing verification secret")
+    const isVerified = speakeasy.totp.verify({
+      secret: user.verificationSecret,
+      encoding: "base32",
+      token,
+    });
+    if (!isVerified)
+      return next(
+        ApiError.badRequest(
+          400,
+          req.originalUrl,
+          "Invalid Time-based OTP provided"
+        )
+      );
+
+    //generate token for user session
+    const { accessToken, refreshToken } = generateToken(user._id, user.email);
+
+    //set user as verified not email
+    user.setIsVerified(false);
+    user.refreshToken = refreshToken;
+    await user.save();
+
+    //hide refresh token after making them visible in query so that it doesnt appear in response
+    user.refreshToken = undefined;
+    user.verificationSecret = undefined;
+
+    //configure and send cookie
+    configureAndSendCookie(res, accessToken, refreshToken);
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          { user: user, accessToken },
+          "Logged in successfully"
+        )
+      );
+  }
+);
 
 const logOut = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
+  async (req: ProtectedRequest, res: Response, next: NextFunction) => {
     //check refresh token in cookie
-    const refreshToken = req.cookies.RefreshToken;
+    const { RefreshToken: refreshToken } = req.cookies;
     //if it does not exist
     if (!refreshToken)
-      return next(ApiError.badRequest(400, "No refresh token provided"));
+      return next(
+        ApiError.badRequest(
+          400,
+          `${req.originalUrl}`,
+          "No refresh token provided"
+        )
+      );
 
     //check if the refresh token sent by the client matches  secret
     //to ensure the compiler doesnt throw error when trying to access a property after decoding the token we ensure the func returns an object of type JwtPayload
@@ -117,10 +291,19 @@ const logOut = asyncHandler(
       REFRESH_TOKEN_SECRET as string
     ) as JwtPayload;
     if (!decodeToken)
-      return next(ApiError.badRequest(400, "Invalid refresh token"));
+      return next(
+        ApiError.badRequest(400, `${req.originalUrl}`, "Invalid refresh token")
+      );
     //delete refresh token from database
     const user = await User.findById(decodeToken.userId);
-    if (!user) return next(ApiError.notFound (404, "User doesn't not exist"));
+    if (!user)
+      return next(
+        ApiError.notFound(
+          404,
+          `${req.originalUrl}`,
+          "Account doesn't not exist"
+        )
+      );
     user.refreshToken = "";
     await user.save();
 
@@ -141,7 +324,13 @@ const tokenRefresh = asyncHandler(
     const { RefreshToken } = req.cookies;
 
     if (!RefreshToken) {
-      return next(ApiError.badRequest(400, "No refresh token provided"));
+      return next(
+        ApiError.badRequest(
+          400,
+          `${req.originalUrl}`,
+          "No refresh token provided"
+        )
+      );
     }
     //verify refresh token
     const decodeToken = jwt.verify(
@@ -149,10 +338,21 @@ const tokenRefresh = asyncHandler(
       process.env.REFRESH_TOKEN_SECRET as string
     ) as JwtPayload;
 
-    const user = await User.findById(decodeToken.userId).select("+refreshToken +email");
-    if (!user) return next(ApiError.notFound(404, "User doesn't not exist"));
+    const user = await User.findById(decodeToken.userId).select(
+      "+refreshToken +email"
+    );
+    if (!user)
+      return next(
+        ApiError.notFound(404, `${req.originalUrl}`, "User doesn't not exist")
+      );
     if (user.refreshToken !== RefreshToken) {
-      return next(ApiError.unAuthorizedRequest(401, "Invalid refresh token"));
+      return next(
+        ApiError.unAuthorizedRequest(
+          401,
+          `${req.originalUrl}`,
+          "Invalid refresh token provided"
+        )
+      );
     }
 
     //generate new access & refresh token
@@ -166,13 +366,15 @@ const tokenRefresh = asyncHandler(
     //configure and send cookie
     configureAndSendCookie(res, accessToken, newRefreshToken);
 
-    return res.status(201).json(
-      new ApiResponse(
-        201,
-        { accessToken },
-        "Access token refreshed successfully"
-      )
-    );
+    return res
+      .status(201)
+      .json(
+        new ApiResponse(
+          201,
+          { accessToken },
+          "Access token refreshed successfully"
+        )
+      );
   }
 );
 
@@ -181,28 +383,33 @@ const configureAndSendCookie = (
   accessToken: string,
   refreshToken: string
 ) => {
-  res.cookie("AccessToken", accessToken, {
-    httpOnly: true, //prevent xss attacks
-    maxAge: 15 * 60 * 1000, //15min
-    secure: NODE_ENV === "production",
-    sameSite: "strict", //CSRF cross site resource forgery attack
-  })
-  .cookie("RefreshToken", refreshToken, {
-    httpOnly: true, //prevent xss attacks
-    secure: NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: 24 * 60 * 60 * 1000, //1day
-  });
+  res
+    .cookie("AccessToken", accessToken, {
+      httpOnly: true, //prevent xss attacks
+      maxAge: 15 * 60 * 1000, //15min
+      secure: NODE_ENV === "production",
+      sameSite: "strict",
+    })
+    .cookie("RefreshToken", refreshToken, {
+      httpOnly: true, //prevent xss attacks
+      secure: NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 24 * 60 * 60 * 1000, //1day
+    });
 };
 
-const generateToken = (userId: string, userEmail: string, OTP?: string) => {
+const generateToken = (
+  userId: string,
+  userEmail: string,
+  code?: number | string
+) => {
   const accessToken = jwt.sign(
     //header -> signing algorithm and token type
     //payload
     {
       userId,
       userEmail,
-      OTP,
+      code,
     },
     //signing secret
     ACCESS_TOKEN_SECRET as string,
@@ -212,33 +419,51 @@ const generateToken = (userId: string, userEmail: string, OTP?: string) => {
       issuer: APP_NAME,
       subject: "Authentication",
     }
-  ) ;
+  );
 
-  const refreshToken = jwt.sign(
-    { userId },
-    REFRESH_TOKEN_SECRET as string,
+  const refreshToken = jwt.sign({ userId }, REFRESH_TOKEN_SECRET as string, {
+    expiresIn: REFRESH_TOKEN_EXPIRY || "1d",
+    issuer: APP_NAME,
+    subject: "Authentication",
+  });
+
+  const verificationToken = jwt.sign(
+    //header
+    //payload
     {
-      expiresIn: REFRESH_TOKEN_EXPIRY || "1d",
+      userId,
+      userEmail,
+      code,
+    },
+    //secret
+    VERIFICATION_TOKEN_SECRET as string,
+    //sign options
+    {
+      expiresIn: VERIFICATION_TOKEN_EXPIRY || ("15m" as string),
       issuer: APP_NAME,
-      subject: "Authentication",
+      subject: "Verification",
     }
   );
 
-  return { accessToken, refreshToken };
+  return { accessToken, refreshToken, verificationToken };
 };
 /**
  * controllers receive request from routers, validate the request, call services and return a response
  */
 
-const sendEmail = async (receiver: string, subject: string, text: string) => {
-  const {data, error} = await resend.emails.send({
-    from: EMAIL_ADDRESS as string,
+const sendEmail = async (
+  receiver: string,
+  appName: string,
+  title: string,
+  code: string | number
+) => {
+  const { data, error } = await resend.emails.send({
+    from: "onboarding@resend.dev",
     to: receiver,
-    subject: subject,
-    html: `<p style="font-size: 16px">${text}</p>`
-  })
-   return {data, error}
+    subject: title,
+    html: generateEmailTemplate(appName, title, code),
+  });
+  return { data, error };
+};
 
-}
-export { signUp, logIn, logOut, tokenRefresh };
-// console.log(EMAIL_ADDRESS)
+export { signUp, logIn, logOut, tokenRefresh, verifyEmail, verifyMFA };
